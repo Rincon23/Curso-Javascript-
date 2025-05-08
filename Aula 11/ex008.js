235868017989174ac086ff4ab1e47750bd5e287a
Enzo Rincon <enzorincon2003@gmail.com>
2025-05-08 15:48:28 -0300

    Aula 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+var vel = 3;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+
+if (vel > 60) { // condição simples
+console.log(`Você ultrapassou a velocidade permitida de 60km/h`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
