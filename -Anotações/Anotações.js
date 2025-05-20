eaca9e74dc31e47cf82279e847ab6b3d4d60bf0b
Enzo Rincon <enzorincon2003@gmail.com>
2025-05-20 00:09:00 -0300

    Aula 16

--- -Anotações/Anotações.js
+++ -Anotações/Anotações.js
@@ -168,6 +168,52 @@ if (pos == -1){
     console.log(`o valor 5 está na posição: ${pos}`)
     }
 
+//Function com parametros
+function parimpar(n) {
+
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(3))
+
+
+function soma(n1, n2) {
+    return n1+n2
+}
+
+console.log(soma(3,4))
+
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(2))
+
+function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1 ; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
+
+// RECURSIVIDADE
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+
+}
+
+console.log(fatorial(5))
 
 
 
